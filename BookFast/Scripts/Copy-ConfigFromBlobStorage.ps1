e8d8f002b530625d947ac2768b71a9a2ad52f7dc
Andrei Dzimchuk <andrei@dzimchuk.net>
2017-05-03 11:10:36 +0300

    prepared for VSTS

---
+++ BookFast/Scripts/Copy-ConfigFromBlobStorage.ps1
@@ -0,0 +1,20 @@
+﻿Param(
+    [String]$destination,
+    [String]$connectionString,
+    [String]$containerName,
+    [String]$prefix
+    )
+
+$context = New-AzureStorageContext -ConnectionString $connectionString
+
+Get-AzureStorageContainer -Context $context -Name $containerName | Get-AzureStorageBlob -Prefix $prefix | % {
+    if (!(Test-Path $destination))
+    {
+        New-Item -Path $destination -ItemType directory -Force
+    }
+    
+    $fileName = Join-Path $destination (Split-Path $_.Name -Leaf)
+    Write-Host $fileName
+    
+    Get-AzureStorageBlobContent -Context $context -Container $containerName -Blob $_.Name -Destination $fileName -Force | Out-Null
+}
