263f14e7f02314176241b5da334464a1d443c52e
Andrei Dzimchuk <andrei@dzimchuk.net>
2017-06-21 09:08:02 +0300

    updated circuit breaker implementation

---
+++ BookFast.Web.Proxy/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Rest;
+using Polly.CircuitBreaker;
+
+namespace BookFast.Web.Proxy
+{
+    internal static class Extensions
+    {
+        public static int StatusCode(this HttpOperationException exception)
+        {
+            return (int)exception.Response.StatusCode;
+        }
+
+        public static int StatusCode(this BrokenCircuitException exception)
+        {
+            return StatusCode((HttpOperationException)exception.InnerException);
+        }
+    }
+}
